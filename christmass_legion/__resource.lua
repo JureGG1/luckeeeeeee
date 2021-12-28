dce9daefe90380c44ec80ea27eff0b3880fb6e09
JureGG1 <88515323+JureGG1@users.noreply.github.com>
2021-12-28 12:52:34 +0100

    Add files via upload

---
+++ christmass_legion/__resource.lua
@@ -0,0 +1,13 @@
+this_is_a_map 'yes'
+
+--[[
+LEAKED BY STAR LEAKS
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+https://discord.gg/XHTBdFcfNP
+]]
